98ac0382a4a3ec4114a192993b2e4a92feac4fce
munotoki <munotoki@gmail.com>
2014-09-01 01:50:19 +0900

    1.000.20140825

---
+++ README.md
@@ -0,0 +1,173 @@
+# プログラミング用フォント Myrica  
+Version 1.000.20140825  
+  
+  
+Myrica （ミリカ）は、フリーなプログラミング用 TrueType フォントです。  
+視認性、判別性 が高くなるように、複数のフォントファイルを元に合成しました。
+  
+  
+## フォントイメージ  
+![Font Image 12p](/Source/FontImage12pHidemaru.png)
+![Font Image 26p](/Source/FontImage26pChrome.png)
+  
+  
+## フォントの特徴  
+多くの特徴をプログラミング用フォント Ricty から継承しています。  
+*ラテン文字には「Inconsolata」が適用されます。  
+*それ以外の文字には「Mgen+」または「Migu 1M」が適用されます。美しい M+ と視認性の高い日本語文字 (半濁音など) が使用できます。  
+*半角文字と全角文字の横幅の比が 1:2 に調整されています。  
+  
+  
+## フォント構成  
+  
+・Myrica.ttc ： 以下の３フォントをパッケージしたファイルです。  
+  
+・MyricaM ： 等幅フォントです。プログラミング用に作成しました。  
+　　　ASCII文字はヒンティングされていますので、Windowsでもそれなりに綺麗に表示されます。  
+  
+・MyricaP ： なんちゃってプロポーショナルフォントです。  
+　　　グリフはそのままに、文字の左右スペースを削り、プロポーショナル文字っぽく作成しました。  
+　　　ただし、数字(0-9)は幅固定していますので、エクセルで数値表示しても、桁がずれません。  
+  
+・MyricaN ： 幅を狭くしたフォントです。一定幅で多くの文字を表示できるように作成しました。  
+　　　グリフは、半角文字:85%、全角ひらがなカタカナ:50%、その他の全角文字:60% に縮小し、  
+　　　文字の左右スペースを削り、プロポーショナル文字っぽく作成しました。  
+　　　ただし、数字(0-9)は幅固定していますので、エクセルで数値表示しても、桁がずれません。  
+  
+  
+## 頒布元  
+
+Myrica の最新版は、以下のサイトで頒布しています。  
+
+GitHub
+https://github.com/munotoki/Myrica
+
+
+## フォントの作成経緯  
+このフォントは、プログラミング用フォント Ricty に感銘を受けて、作成したものです。  
+  
+Ricty はとても素晴らしいフォントですが、幾つかの不満点がありました。  
+・ヒンティングされていないので、環境によっては半角英数字が潰れてしまい判別しにくくなる場合がある。  
+・M+ OUTLINE FONTS 以外の日本語フォントが IPA 由来のため、Inconsolata との合成フォントが再配布できない。  
+・アスタリスク(*) の字形が好みではない。  
+  
+これらに関して、以下のように解決したのが、本フォント Myrica です。  
+・ヒンティング付きの Inconsolata を使用し、ヒンティングを消さないように合成した。  
+・M+ OUTLINE FONTS 以外のフォントを IPA から 源ノ角ゴシック に変更して再配布可能とした。  
+・アスタリスク(*) を含む幾つかを好みのオリジナルな字形に変更した。  
+  
+ただし、ASCII文字(0x21-0x7E) 以外については、ヒンティングはありません。  
+  
+  
+## グリフの構成  
+プログラミング用に文字が判別しやすいように考慮しています。
+
+(1)ASCII文字(0x21-0x7E) を「Inconsolata」で置換えました。  
+  さらに以下の変更を行っています。  
+  ・r にセリフ付き (Inconsolata に内包されているグリフで置換え)  
+  ・D にクロスバー付き (Inconsolata に内包されているグリフで置換え)  
+  ・| が破断線 (Inconsolata に内包されているグリフで置換え)  
+  ・"'`,.:; が大きめに作られている (修正した)  
+※Inconsolata: 「Top 10 Programming Fonts」や「プログラミング時に最適なフォント『Inconsolata』」などで高い評価を受けているサンセリフ体等幅欧文フォント  
+  
+  
+(2)ASCII文字以外は「Mgen+」をベースとしました。  
+※Mgen+: 「M+ OUTLINE FONTS」と「源ノ角ゴシック」をベースとして作成された日本語フォント  
+  
+  
+さらに、一部の文字は他の文字に置き換えています。  
+(3)「Migu」由来の文字への置換え。  
+  ・半濁点（ぱぴぷぺぽパピプペポ の右上の円）を大きくして、濁点と判別しやすく。  
+  ・「カ力 エ工 ロ口 ー一 ニ二」（カタカナ・漢字）の区別。  
+  ・ ～〜（FULLWIDTH TILDE・WAVE DASH）の区別。  
+  ・αβなど一部のギリシャ文字、φЯなど一部のロシア文字を全角に。  
+※Migu: 「M+ OUTLINE FONTS」をベースに改良された視認性の高い日本語文字を含むフォント  
+  
+  
+(4)「オリジナル」な文字への置換え  
+  ・0x002a   * : astarisk  
+  ・0x0030   0 : zero  
+  ・0x006c   l : small letter l  
+  ・0x2013   – : en dash –  
+  ・0x2014   — : em dash —  
+  
+  
+## ライセンスと著作権について  
+「SIL OPEN FONT LICENSE Version 1.1」と「Apache 2.0 License」のデュアルライセンスです。
+  
+・ASCII文字は、Inconsolata と同じ SIL OPEN FONT LICENSE Version 1.1 のもとで使用することができます。  
+・ASCII文字以外 は、Megn+ と同じ Apache 2.0 License のもとで使用することができます。  
+・ただし、使用や埋め込み・改変を行った結果、使用したのが M+ OUTLINE FONTS 由来の文字グリフのみだった場合は、より要件が少ない M+ FONT LICENSE を適用することができます。  
+  
+SIL OPEN FONT LICENSE Version 1.1 の内容は、アーカイブに同梱の LICENSE_OFL.txt に記載されています。  
+この日本語訳は、以下から参照することができます。  
+http://sourceforge.jp/projects/opensource/wiki/SIL_Open_Font_License_1.1  
+  
+Apache 2.0 License の内容は、アーカイブに同梱の LICENSE_Apache.txt に記載されています。  
+この日本語訳は、以下から参照することができます。  
+http://sourceforge.jp/projects/opensource/wiki/licenses%2FApache_License_2.0  
+  
+M+ OUTLINE FONTS のグリフは、同梱のファイル LICENSE_M+.txt に記載された自由なM+ FONTS LICENSE に基づき使用しています  
+  
+  
+・Myrica は、M+ OUTLINE FONTS にないグリフを源ノ角ゴシックで補い、ASCII文字をInconsolataで置換えたフォントです。  
+・フォントデータに含まれる、源ノ角ゴシック由来の文字グリフの著作権は Adobe が所有しています。  
+・フォントデータに含まれる、M+ OUTLINE FONTS 由来の文字グリフの著作権は M+ FONTS PROJECT が所有しています。  
+・フォントデータに含まれる、Inconsolata 由来の文字グリフの著作権は Raph Levien, Cyreal が所有しています。  
+  
+  
+## M+ 由来の文字について  
+---- Mgen+ の README より抜粋 ----  
+> このバージョンでは、M+ OUTLINE FONTS 2014 年 8 月 3 日時点での CVS 版をベースとしています。  
+> TESTFLIGHT-058 の漢字に加え、新たに制作された以下の漢字を含んでいます。  
+>  
+> 瘴嬲嫐娶夥埒嚼嚥臙讌叭釟冪匍匐僥潛僭譖獰湜懿點鈞繼爀璇騫勳粱岺岑邙玟泮悳乭儺攤柰  
+> 璨娜拏贊讚鑽瓚盡儘燼贐賣囮皺鄒猜睛蜻闢  
+>  
+> M+ 由来の漢字グリフは 4,859 字がこのフォントに収録されており、それ以外の漢字は源ノ角ゴシックがベースとなっています。  
+  
+  
+## 名前の由来  
+・Mgen+ 作者の MM氏  
+・Ricty 作者の Yusa 氏  
+・Inconsolata 作者の Raph Levien 氏  
+・Migu 作者の itouhiro 氏  
+・M+ 作者の coz 氏  
+・源の角ゴシック 作者の Adobe Systems Software Ireland Ltd.  
+それぞれの作者の頭文字から Myrica と命名しました。  
+  
+  
+## 改変元  
+  
+Myrica は、以下のフォントを直接または間接的に合成、参照して制作しました。  
+素晴らしいフリーフォントの制作に関わる全ての方に深くお礼申し上げます。  
+  
+M+ OUTLINE FONTS  
+http://mplus-fonts.sourceforge.jp/  
+  
+Mgen+  
+http://jikasei.me/font/mgenplus/  
+  
+源ノ角ゴシック (Source Hans Sans)  
+http://store1.adobe.com/cfusion/store/html/index.cfm?store=OLS-JP&event=displayFontPackage&code=1967  
+  
+Migu  
+http://mix-mplus-ipa.sourceforge.jp/migu/  
+  
+Inconsolata  
+https://www.google.com/fonts/specimen/Inconsolata  
+  
+Ricty  
+https://github.com/yascentur/Ricty  
+  
+  
+## 改版履歴  
+  
+  
+#### Version 1.000.20140825  
+  
+・初回リリース  
+・以下のファイルを元に合成  
+　　Inconsolata : Inconsolata-Regular.ttf : 1.013 (Google Fonts)  
+　　Mgen+       : mgenplus-1m-regular.ttf : 1.058.20140808 (20140808)  
+　　Migu        : migu-1m-regular.ttf     : 2013.0617 (20130617)  
---
+++ Source/myrica_generator.py
@@ -0,0 +1,524 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+# Author: Tomokuni SEKIYA
+#
+# This script is for generating ``myrica'' font
+#
+# * Inconsolata : Inconsolata-Regular.ttf : 1.013 (Google Fonts)
+# * (original)  : ReplaceParts.ttf        : 1.000.20140825
+# * Mgen+       : mgenplus-1m-regular.ttf : 1.058.20140808 (20140828)
+# * Migu        : migu-1m-regular.ttf     : 2013.0617 (20130617)
+#
+#以下のように構成されます。
+#・英数字記号は、Inconsolata-Regular.ttf と ReplaceParts.ttf
+#・他の文字は、mgenplus-1m-regular.ttf
+#・一部の文字は、circle-mplus-1m-regular.ttf
+#    対象：・半濁点（ぱぴぷぺぽパピプペポ の右上の円）を大きくして、濁点と判別しやすく
+#          ・「カ力 エ工 ロ口 ー一 ニ二」（カタカナ・漢字）の区別
+#          ・～〜（FULLWIDTH TILDE・WAVE DASH）の区別
+#          ・αβなど一部のギリシャ文字、φЯなど一部のロシア文字を全角に
+#          ・×÷±などの数学記号を全角に
+#
+
+# version
+newfont_version      = "1.001.20140829"
+newfont_sfntRevision = 0x00010000
+
+# flag
+scalingDownIfWidth_flag = True
+
+# set font name
+newfontM = ("../../MyricaSourceTTF/MyricaM.ttf", "MyricaM", "Myrica M", "Myrica Monospace")
+newfontP = ("../../MyricaSourceTTF/MyricaP.ttf", "MyricaP", "Myrica P", "Myrica Proportional")
+newfontN = ("../../MyricaSourceTTF/MyricaN.ttf", "MyricaN", "Myrica N", "Myrica Narrow")
+
+# set ascent and descent (line width parameters)
+newfont_ascent  = 840
+newfont_descent = 184
+newfont_em = newfont_ascent + newfont_descent
+
+# source file
+srcfontIncosolata   = "../../MyricaSourceTTF/Inconsolata-Regular.ttf"
+srcfontMgenplus     = "../../MyricaSourceTTF/mgenplus-1m-regular.ttf"
+srcfontMigu         = "../../MyricaSourceTTF/migu-1m-regular.ttf"
+srcfontReplaceParts = "ReplaceParts.ttf"
+
+# define
+generate_flags = ('opentype', 'PfEd-lookups', 'TeX-table')
+panoseBase = (2, 11, 5, 9, 2, 2, 3, 2, 2, 7)
+
+########################################
+# setting
+########################################
+
+import copy
+import os
+import sys
+import fontforge
+import psMat
+
+# 縦書きのために指定する
+fontforge.setPrefs('CoverageFormatsAllowed', 1)
+# 大変更時に命令を消去 0:オフ 1:オン
+fontforge.setPrefs('ClearInstrsBigChanges', 0)
+# TrueType命令をコピー 0:オフ 1:オン
+fontforge.setPrefs('CopyTTFInstrs', 1)
+
+########################################
+# pre-process
+########################################
+
+print 
+print 
+print "myrica generator " + newfont_version
+print 
+print "This script is for generating 'myrica' font"
+print 
+if os.path.exists( srcfontIncosolata ) == False:
+    print "Error: " + srcfontIncosolata + " not found"
+    sys.exit( 1 )
+if os.path.exists( srcfontReplaceParts ) == False:
+    print "Error: " + srcfontReplaceParts + " not found"
+    sys.exit( 1 )
+if os.path.exists( srcfontMgenplus ) == False:
+    print "Error: " + srcfontMgenplus + " not found"
+    sys.exit( 1 )
+if os.path.exists( srcfontMigu ) == False:
+    print "Error: " + srcfontMigu + " not found"
+    sys.exit( 1 )
+
+########################################
+# define function
+########################################
+def matRescale(origin_x, origin_y, scale_x, scale_y):
+    return psMat.compose(
+        psMat.translate(-origin_x, -origin_y), psMat.compose(
+        psMat.scale(scale_x, scale_y), 
+        psMat.translate(origin_x, origin_y)))
+
+def matMove(move_x, move_y):
+    return psMat.translate(move_x, move_y)
+
+def flatten(iterable):
+    it = iter(iterable)
+    for e in it:
+        if isinstance(e, (list, tuple)):
+            for f in flatten(e):
+                yield f
+        else:
+            yield e
+
+def rng(start, end):
+    return range(start, end + 1)
+
+def scalingDownIfWidth(font, if_width):
+    for glyph in font.glyphs():
+        if glyph.width == if_width:
+            glyph.transform(matRescale(if_width / 2, 0, 0.91, 0.91))
+            glyph.width = if_width
+
+def copyAndPaste(srcFont, srcCodes, dstFont, dstCodes):
+    select(srcFont, srcCodes)
+    srcFont.copy()
+    select(dstFont, dstCodes)
+    dstFont.paste()
+
+def copyAndPasteInto(srcFont, srcCode, dstFont, dstCode, pos_x, pos_y):
+    srcFont.selection.select(("more",), srcCode)
+    srcFont.copy()
+    dstFont.transform(matMove(-pos_x, -pos_y))
+    dstFont.pasteInto()
+    dstFont.transform(matMove(pos_x, pos_y))
+
+def select(font, *codes):
+    font.selection.none()
+    flat = flatten(codes)
+    for c in flat:
+        font.selection.select(("more",), c)
+
+def selectMore(font, *codes):
+    flat = flatten(codes)
+    for c in flat:
+        font.selection.select(("more",), c)
+
+def centerInWidth(font):
+    for glyph in font.selection.byGlyphs:
+        w  = glyph.width
+        wc = w / 2
+        bb = glyph.boundingBox()
+        bc = (bb[0] + bb[2]) / 2
+        glyph.transform(matMove(wc - bc, 0))
+        glyph.width = w
+
+def setWidth(font, width):
+    for glyph in font.selection.byGlyphs:
+        glyph.width = width
+
+def setAutoWidth(font, separation):
+    for glyph in font.selection.byGlyphs:
+        bb = glyph.boundingBox()
+        bc = (bb[0] + bb[2]) / 2
+        nw = (bb[2] - bb[0]) + separation * 2
+        wc = nw / 2
+        glyph.transform(matMove(wc - bc, 0))
+        glyph.width = nw
+
+def setAutoWidthGlyph(glyph, separation):
+        bb = glyph.boundingBox()
+        bc = (bb[0] + bb[2]) / 2
+        nw = (bb[2] - bb[0]) + separation * 2
+        wc = nw / 2
+        glyph.transform(matMove(wc - bc, 0))
+        glyph.width = nw
+
+########################################
+# modified Inconsolata
+########################################
+
+print
+print "Open " + srcfontIncosolata
+fIn = fontforge.open( srcfontIncosolata )
+
+# modify
+print "modify"
+# 拡大 "'`
+select(fIn, 0x0022, 0x0027, 0x0060)
+fIn.transform(matRescale(250, 600, 1.15, 1.15))
+setWidth(fIn, 1000 / 2)
+
+# 拡大 ,.:;
+select(fIn, 0x002c, 0x002e, 0x003a, 0x003b)
+fIn.transform(matRescale(250, 0, 1.15, 1.15))
+setWidth(fIn, 1000 / 2)
+
+# 移動 +-<>=
+select(fIn, 0x002b, 0x002d, 0x003c, 0x003e, 0x003d)
+fIn.transform(matMove(0, -80))
+setWidth(fIn, 1000 / 2)
+
+# 移動 ~
+select(fIn, 0x007e)
+fIn.transform(matMove(0, 120))
+
+# 移動 ()
+select(fIn, 0x0028, 0x0029)
+fIn.transform(matMove(0, 78))
+
+# 移動 []
+select(fIn, 0x005b, 0x005d)
+fIn.transform(matMove(0, 44))
+
+# 移動 {}
+select(fIn, 0x007b, 0x007d)
+fIn.transform(matMove(0, 99))
+
+# | -> broken | (Inconsolata's glyph)
+copyAndPaste(fIn, 0x00a6, fIn, 0x007c)
+
+# D -> D of Eth (D with cross-bar)
+copyAndPaste(fIn, 0x0110, fIn, 0x0044)
+
+# r -> r of serif (Inconsolata's unused glyph)
+copyAndPaste(fIn,  65548, fIn, 0x0072)
+
+# 必要文字(半角英数字記号)だけを残して削除 # 
+select(fIn, rng(0x0021, 0x007E))
+fIn.selection.invert()
+fIn.clear()
+
+# modify em
+fIn.em  = newfont_em
+fIn.ascent  = newfont_ascent
+fIn.descent = newfont_descent
+
+setWidth(fIn, newfont_em / 2)
+
+#fIn.generate("/modIncosolata.ttf", '', generate_flags)
+
+########################################
+# modified ReplaceParts
+########################################
+
+print
+print "Open " + srcfontReplaceParts
+fPa = fontforge.open( srcfontReplaceParts )
+
+# modify em
+fPa.em  = newfont_em
+fPa.ascent  = newfont_ascent
+fPa.descent = newfont_descent
+
+# post-process
+fPa.selection.all()
+fPa.round()
+
+#fPa.generate("/modReplaceParts.ttf", '', generate_flags)
+
+########################################
+# modified Mgen+ 1m
+########################################
+
+print
+print "Open " + srcfontMgenplus
+fMg = fontforge.open( srcfontMgenplus )
+
+## modify em
+#fMg.em  = newfont_em
+#fMg.ascent  = newfont_ascent
+#fMg.descent = newfont_descent
+#
+#fMg.generate("/modMgenplus.ttf", '', generate_flags)
+
+########################################
+# modified Migu 1m
+########################################
+
+print
+print "Open " + srcfontMigu
+fMi = fontforge.open( srcfontMigu )
+
+# modify em
+fMi.em  = newfont_em
+fMi.ascent  = newfont_ascent
+fMi.descent = newfont_descent
+
+#fMi.generate("/modMigu.ttf", '', generate_flags)
+
+########################################
+# create MyricaM
+########################################
+fRm = fIn
+
+print
+print "Build " + newfontM[0]
+
+# pre-process
+fRm.fontname = newfontM[1]
+fRm.familyname = newfontM[2]
+fRm.fullname = newfontM[3]
+fRm.weight = "Book"
+fRm.copyright =  "Copyright (c) 2006-2012 Raph Levien (Inconsolata)\n"
+fRm.copyright += "Copyright (c) 2013 itouhiro (Circle M+)\n"
+fRm.copyright += "Copyright (c) 2013 M+ FONTS PROJECT (M+)\n"
+fRm.copyright += "Copyright (c) 2014 MM (Mgen+)\n"
+fRm.copyright += "Copyright (c) 2014 Adobe Systems Incorporated. (NotoSansJP)\n"
+fRm.copyright += "Licenses:\n"
+fRm.copyright += "SIL Open Font License Version 1.1 "
+fRm.copyright += "(http://scripts.sil.org/ofl)\n"
+fRm.copyright += "Apache License, Version 2.0 "
+fRm.copyright += "(http://www.apache.org/licenses/LICENSE-2.0)"
+fRm.version = newfont_version
+fRm.sfntRevision = newfont_sfntRevision
+fRm.sfnt_names = ()
+
+fRm.os2_panose = panoseBase
+fRm.os2_vendor = fMg.os2_vendor
+fRm.os2_version = 1
+
+fRm.os2_winascent = newfont_ascent
+fRm.os2_winascent_add = 0
+fRm.os2_windescent = newfont_descent
+fRm.os2_windescent_add = 0
+fRm.os2_typoascent = newfont_ascent
+fRm.os2_typoascent_add = 0
+fRm.os2_typodescent = -newfont_descent
+fRm.os2_typodescent_add = 0
+fRm.os2_typolinegap
+fRm.hhea_ascent = newfont_ascent
+fRm.hhea_ascent_add = 0
+fRm.hhea_descent = -newfont_descent
+fRm.hhea_descent_add = 0
+fRm.hhea_linegap = 0
+
+# marge ReplaceParts
+print "marge ReplaceParts"
+target = (
+    0x002a,  # * : astarisk
+    0x0030,  # 0 : zero
+    0x006c,  # l : small letter l
+    0x2013,  # – : en dash –
+    0x2014)  # — : em dash —
+copyAndPaste(fPa, target, fRm, target)
+
+# marge Mgen+ 1m
+print "marge Mgen+ 1m"
+fRm.mergeFonts( srcfontMgenplus )
+fRm.os2_unicoderanges = fMg.os2_unicoderanges
+fRm.os2_codepages = fMg.os2_codepages
+for l in fMg.gsub_lookups:
+    if (l in fRm.gsub_lookups) == False:
+        fRm.importLookups(fMg, l)
+for l in fRm.gsub_lookups:
+    if l.startswith(fMg.fontname + "-") == True:
+        fRm.removeLookup(l)
+
+# marge Migu 1m
+print "marge Migu 1m"
+target = (
+    # 半濁点（ぱぴぷぺぽパピプペポ の右上の円）を大きくして、濁点と判別しやすく
+    (0x3071, 0x3074, 0x3077, 0x307A, 0x307D, 0x30D1, 0x30D4, 0x30D7, 0x30DA, 0x30DD),
+    # 「カ力 エ工 ロ口 ー一 ニ二」（カタカナ・漢字）の区別
+    (0x30AB, 0x529B, 0x30A8, 0x5DE5, 0x30ED, 0x53E3, 0x30FC, 0x4E00, 0x30CB, 0x4E8C),
+    # ～〜（FULLWIDTH TILDE・WAVE DASH）の区別
+    (0xFF5E, 0x301C),
+    # αβなど一部のギリシャ文字、φЯなど一部のロシア文字を全角に
+    (rng(0x0391, 0x03a9), rng(0x03b1, 0x03c9)),
+    (0x0401, rng(0x0410, 0x044F), 0x0451))
+copyAndPaste(fMi, target, fRm, target)
+
+# modify
+print "modify"
+
+# scaling down
+if scalingDownIfWidth_flag == True:
+    print "While scaling, wait a little..."
+    scalingDownIfWidth(fRm, newfont_em)
+
+
+# 全角 comma and period ，．
+select(fRm, 0xff0c, 0xff0e)
+fRm.transform(matRescale(102, 0, 1.54, 1.54))
+setWidth(fRm, newfont_em)
+
+# 全角 colon and semicolon ：；
+copyAndPaste    (fRm, 0xff0c, fRm, 0xff1b)
+copyAndPasteInto(fRm, 0xff0e, fRm, 0xff1b, 0, 410)
+copyAndPaste    (fRm, 0xff0e, fRm, 0xff1a)
+copyAndPasteInto(fRm, 0xff0e, fRm, 0xff1a, 0, 410)
+select(fRm, 0xff1b, 0xff1a)
+centerInWidth(fRm)
+
+# 全角 brackets （）［］｛｝＜＞
+srcTarget = (0x0028, 0x0029, 0x005b, 0x005d, 0x007b, 0x007d, 0x003c, 0x003e)
+dstTarget = (0xff08, 0xff09, 0xff3b, 0xff3d, 0xff5b, 0xff5d, 0xff1c, 0xff1e)
+copyAndPaste(fRm, srcTarget, fRm, dstTarget)
+fRm.transform(matMove(256, 0))
+setWidth(fRm, newfont_em)
+
+# post-process
+fRm.selection.all()
+fRm.round()
+
+print "Generate " + newfontM[0]
+fRm.generate(newfontM[0], '', generate_flags)
+#fRm.close()
+fPa.close()
+fMg.close()
+fMi.close()
+
+########################################
+# create MyricaP
+########################################
+print
+print "Build " + newfontP[0]
+fRp = fontforge.open( newfontM[0] )
+
+# pre-process
+fRp.fontname = newfontP[1]
+fRp.familyname = newfontP[2]
+fRp.fullname = newfontP[3]
+
+panose = list(fRp.os2_panose)
+panose[3] = 0
+fRp.os2_panose = tuple(panose)
+
+# modify
+print "modify"
+
+# 半角
+hankaku = (100,)
+# 全角
+zenkaku = (50,)
+
+# 全文字の幅の自動設定
+fRp.selection.all()
+for glyph in fRp.selection.byGlyphs:
+    #print glyph.glyphname + ", code " + str(glyph.unicode) + ", width " + str(glyph.width)
+    if glyph.width == newfont_em:         # 全角文字
+        setAutoWidthGlyph(glyph, hankaku[0])
+    else:                                 # 半角文字
+        bb = glyph.boundingBox()
+        nw = (bb[2] - bb[0]) + zenkaku[0] * 2
+        if glyph.width > nw:
+            setAutoWidthGlyph(glyph, zenkaku[0])
+
+# 半角数字は幅固定で中央配置
+select(fRp, rng(0x0030,0x0039))   # 0-9
+setWidth(fRp, 490)
+centerInWidth(fRp)
+
+# post-process
+fRp.selection.all()
+fRp.round()
+
+print "Generate " + newfontP[0]
+fRp.generate(newfontP[0], '', generate_flags)
+fRp.close()
+
+########################################
+# create MyricaN
+########################################
+print
+print "Build " + newfontN[0]
+fRn = fontforge.open( newfontM[0] )
+
+# pre-process
+fRn.fontname = newfontN[1]
+fRn.familyname = newfontN[2]
+fRn.fullname = newfontN[3]
+
+panose = list(fRn.os2_panose)
+panose[3] = 0
+fRn.os2_panose = tuple(panose)
+
+# modify
+print "modify"
+
+# 半角英数
+heisu = (0.85, 1.00, 50, (rng(0x0030,0x0039), rng(0x0041,0x005A), rng(0x0061,0x007A)))
+# 半角記号
+hkigo = (0.85, 1.00, 50, (rng(0x0021,0x002F), rng(0x003A,0x0040), rng(0x005B,0x0060), rng(0x007B,0x007E)))
+# 全角かな
+zkana = (0.50, 1.00, 50, rng(0x3041,0x31FF))
+# 半角かな
+hkana = (0.85, 1.00, 50, rng(0xFF66,0xFF9F))
+# その他の全角文字
+zetc  = (0.60, 1.00, 50)
+# その他の半角文字
+hetc  = (0.85, 1.00, 50)
+
+# 拡大縮小 と 幅の自動設定
+fRn.selection.all()
+for glyph in fRn.selection.byGlyphs:
+    #print glyph.glyphname + ", code " + str(glyph.unicode) + ", width " + str(glyph.width)
+    glyph.ttinstrs = ""
+    if glyph.unicode in zkana[3]:           # 全角かな
+        glyph.transform(matRescale(0, 0, zkana[0], zkana[1]))
+        setAutoWidthGlyph(glyph, zkana[2])
+    elif glyph.width == newfont_em:         # その他の全角文字
+        glyph.transform(matRescale(0, 0, zetc[0],  zetc[1]))
+        setAutoWidthGlyph(glyph, zetc[2])
+    else:                                   # 半角文字
+        glyph.transform(matRescale(0, 0, hetc[0],  hetc[1]))
+        bb = glyph.boundingBox()
+        nw = (bb[2] - bb[0]) + hetc[2] * 2
+        if glyph.width > nw:
+            setAutoWidthGlyph(glyph, hetc[2])
+
+# 全文字の幅の自動設定
+fRn.selection.all()
+
+# 半角数字は幅固定で中央配置
+select(fRn, rng(0x0030,0x0039))   # 0-9
+setWidth(fRn, 400)
+centerInWidth(fRn)
+
+# post-process
+fRn.selection.all()
+fRn.round()
+
+print "Generate " + newfontN[0]
+fRn.generate(newfontN[0], '', generate_flags)
+fRn.close()
