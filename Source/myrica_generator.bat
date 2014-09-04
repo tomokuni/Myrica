bef4561e4a22d715139e2a38fc5b0f75ac382448
munotoki <munotoki@gmail.com>
2014-09-04 18:07:03 +0900

    1.003.20140904

--- Source/myrica_generator.bat
+++ Source/myrica_generator.bat
@@ -7,23 +7,17 @@ set TZ=
 set DISPLAY=:9.0
 set AUTOTRACE=potrace
 
-start /B XWin.exe :9 -multiwindow -nomultimonitors -silent-dup-error
+del ..\..\MyricaSourceTTF\MyricaM.ttf
+del ..\..\MyricaSourceTTF\MyricaP.ttf
+del ..\..\MyricaSourceTTF\MyricaN.ttf
 
-del ..\..\MyricaSourceTTF\Myrica*.*
+start /B XWin.exe :9 -multiwindow -nomultimonitors -silent-dup-error
 
 xwin-close.exe -wait
 fontforge.exe  -lang=py -script myrica_generator.py
 xwin-close.exe -close
 
-rem echo .
-rem echo ÉqÉìÉeÉBÉìÉO
-rem ..\..\ttfautohint.exe --hinting-range-min=8 --hinting-range-max=50 --default-script=latn --hinting-limit=200 --increase-x-height=14 --no-info --strong-stem-width=GD ..\..\MyricaSourceTTF\MyricaHMs.ttf ..\..\MyricaSourceTTF\MyricaHM.ttf
-rem ..\..\ttfautohint.exe --hinting-range-min=8 --hinting-range-max=50 --default-script=latn --hinting-limit=200 --increase-x-height=14 --no-info --strong-stem-width=GD ..\..\MyricaSourceTTF\MyricaHPs.ttf ..\..\MyricaSourceTTF\MyricaHP.ttf
-rem ..\..\ttfautohint.exe --hinting-range-min=8 --hinting-range-max=50 --default-script=latn --hinting-limit=200 --increase-x-height=14 --no-info --strong-stem-width=GD ..\..\MyricaSourceTTF\MyricaHNs.ttf ..\..\MyricaSourceTTF\MyricaHN.ttf
-rem del ..\..\MyricaSourceTTF\MyricaH*s.*
-
 echo .
 echo TTCÇÃçÏê¨
 ..\..\UniteTTC.exe ..\Myrica.TTC  ..\..\MyricaSourceTTF\MyricaM.ttf  ..\..\MyricaSourceTTF\MyricaP.ttf  ..\..\MyricaSourceTTF\MyricaN.ttf
-rem ..\..\UniteTTC.exe ..\MyricaH.TTC ..\..\MyricaSourceTTF\MyricaHM.ttf ..\..\MyricaSourceTTF\MyricaHP.ttf ..\..\MyricaSourceTTF\MyricaHN.ttf
 pause
--- Source/myrica_generator.py
+++ Source/myrica_generator.py
@@ -18,12 +18,12 @@
 #    対象：・半濁点（ぱぴぷぺぽパピプペポ の右上の円）を大きくして、濁点と判別しやすく
 #          ・「カ力 エ工 ロ口 ー一 ニ二」（カタカナ・漢字）の区別
 #          ・～〜（FULLWIDTH TILDE・WAVE DASH）の区別
-#          ・αβなど一部のギリシャ文字、φЯなど一部のロシア文字を全角に
-#          ・×÷±などの数学記号を全角に
+#          ・αβなど一部のギリシャ文字、φЯなど一部のキリル文字を全角に
+#          ・±×÷√‰§†‡¶などの記号を全角に
 #
 
 # version
-newfont_version      = "1.002.20140903"
+newfont_version      = "1.003.20140904"
 newfont_sfntRevision = 0x00010000
 
 # flag
@@ -33,10 +33,6 @@ scalingDownIfWidth_flag = True
 newfontM  = ("../../MyricaSourceTTF/MyricaM.ttf",   "MyricaM",  "Myrica M",  "Myrica Monospace")
 newfontP  = ("../../MyricaSourceTTF/MyricaP.ttf",   "MyricaP",  "Myrica P",  "Myrica Proportional")
 newfontN  = ("../../MyricaSourceTTF/MyricaN.ttf",   "MyricaN",  "Myrica N",  "Myrica Narrow")
-
-#newfontHM = ("../../MyricaSourceTTF/MyricaHMs.ttf", "MyricaHM", "MyricaH M", "MyricaH Monospace")
-#newfontHP = ("../../MyricaSourceTTF/MyricaHPs.ttf", "MyricaHP", "MyricaH P", "MyricaH Proportional")
-#newfontHN = ("../../MyricaSourceTTF/MyricaHNs.ttf", "MyricaHN", "MyricaH N", "MyricaH Narrow")
 
 # set ascent and descent (line width parameters)
 newfont_ascent  = 840
@@ -105,6 +101,9 @@ def matRescale(origin_x, origin_y, scale_x, scale_y):
 def matMove(move_x, move_y):
     return psMat.translate(move_x, move_y)
 
+def rng(start, end):
+    return range(start, end + 1)
+
 def flatten(iterable):
     it = iter(iterable)
     for e in it:
@@ -114,14 +113,17 @@ def flatten(iterable):
         else:
             yield e
 
-def rng(start, end):
-    return range(start, end + 1)
+def select(font, *codes):
+    font.selection.none()
+    selectMore(font, codes)
 
-def scalingDownIfWidth(font, if_width):
-    for glyph in font.glyphs():
-        if glyph.width == if_width:
-            glyph.transform(matRescale(if_width / 2, 0, 0.91, 0.91))
-            glyph.width = if_width
+def selectMore(font, *codes):
+    flat = flatten(codes)
+    for c in flat:
+        if isinstance(c, (unicode, )):
+            font.selection.select(("more",), ord(c))
+        else:
+            font.selection.select(("more",), c)
 
 def copyAndPaste(srcFont, srcCodes, dstFont, dstCodes):
     select(srcFont, srcCodes)
@@ -137,14 +139,11 @@ def copyAndPasteInto(srcFont, srcCodes, dstFont, dstCodes, pos_x, pos_y):
     dstFont.pasteInto()
     dstFont.transform(matMove(pos_x, pos_y))
 
-def select(font, *codes):
-    font.selection.none()
-    selectMore(font, codes)
-
-def selectMore(font, *codes):
-    flat = flatten(codes)
-    for c in flat:
-        font.selection.select(("more",), c)
+def scalingDownIfWidth(font, if_width):
+    for glyph in font.glyphs():
+        if glyph.width == if_width:
+            glyph.transform(matRescale(if_width / 2, 0, 0.91, 0.91))
+            glyph.width = if_width
 
 def centerInWidth(font):
     for glyph in font.selection.byGlyphs:
@@ -283,49 +282,49 @@ fMi.descent = newfont_descent
 ########################################
 # create MyricaM
 ########################################
-fRm = fIn
+fMm = fIn
 
 print
 print "Build " + newfontM[0]
 
 # pre-process
-fRm.fontname   = newfontM[1]
-fRm.familyname = newfontM[2]
-fRm.fullname   = newfontM[3]
-fRm.weight = "Book"
-fRm.copyright =  "Copyright (c) 2006-2012 Raph Levien (Inconsolata)\n"
-fRm.copyright += "Copyright (c) 2013 itouhiro (Circle M+)\n"
-fRm.copyright += "Copyright (c) 2013 M+ FONTS PROJECT (M+)\n"
-fRm.copyright += "Copyright (c) 2014 MM (Mgen+)\n"
-fRm.copyright += "Copyright (c) 2014 Adobe Systems Incorporated. (NotoSansJP)\n"
-fRm.copyright += "Licenses:\n"
-fRm.copyright += "SIL Open Font License Version 1.1 "
-fRm.copyright += "(http://scripts.sil.org/ofl)\n"
-fRm.copyright += "Apache License, Version 2.0 "
-fRm.copyright += "(http://www.apache.org/licenses/LICENSE-2.0)"
-fRm.version = newfont_version
-fRm.sfntRevision = newfont_sfntRevision
-fRm.sfnt_names = (('English (US)', 'UniqueID', newfontM[2]), )
-fRm.head_optimized_for_cleartype = True
+fMm.fontname   = newfontM[1]
+fMm.familyname = newfontM[2]
+fMm.fullname   = newfontM[3]
+fMm.weight = "Book"
+fMm.copyright =  "Copyright (c) 2006-2012 Raph Levien (Inconsolata)\n"
+fMm.copyright += "Copyright (c) 2013 itouhiro (Circle M+)\n"
+fMm.copyright += "Copyright (c) 2013 M+ FONTS PROJECT (M+)\n"
+fMm.copyright += "Copyright (c) 2014 MM (Mgen+)\n"
+fMm.copyright += "Copyright (c) 2014 Adobe Systems Incorporated. (NotoSansJP)\n"
+fMm.copyright += "Licenses:\n"
+fMm.copyright += "SIL Open Font License Version 1.1 "
+fMm.copyright += "(http://scripts.sil.org/ofl)\n"
+fMm.copyright += "Apache License, Version 2.0 "
+fMm.copyright += "(http://www.apache.org/licenses/LICENSE-2.0)"
+fMm.version = newfont_version
+fMm.sfntRevision = newfont_sfntRevision
+fMm.sfnt_names = (('English (US)', 'UniqueID', newfontM[2]), )
+fMm.head_optimized_for_cleartype = True
 
-fRm.os2_panose = panoseBase
-fRm.os2_vendor = fMg.os2_vendor
-fRm.os2_version = 1
+fMm.os2_panose = panoseBase
+fMm.os2_vendor = fMg.os2_vendor
+fMm.os2_version = 1
 
-fRm.os2_winascent = newfont_ascent
-fRm.os2_winascent_add = 0
-fRm.os2_windescent = newfont_descent
-fRm.os2_windescent_add = 0
-fRm.os2_typoascent = newfont_ascent
-fRm.os2_typoascent_add = 0
-fRm.os2_typodescent = -newfont_descent
-fRm.os2_typodescent_add = 0
-fRm.os2_typolinegap
-fRm.hhea_ascent = newfont_ascent
-fRm.hhea_ascent_add = 0
-fRm.hhea_descent = -newfont_descent
-fRm.hhea_descent_add = 0
-fRm.hhea_linegap = 0
+fMm.os2_winascent = newfont_ascent
+fMm.os2_winascent_add = 0
+fMm.os2_windescent = newfont_descent
+fMm.os2_windescent_add = 0
+fMm.os2_typoascent = newfont_ascent
+fMm.os2_typoascent_add = 0
+fMm.os2_typodescent = -newfont_descent
+fMm.os2_typodescent_add = 0
+fMm.os2_typolinegap
+fMm.hhea_ascent = newfont_ascent
+fMm.hhea_ascent_add = 0
+fMm.hhea_descent = -newfont_descent
+fMm.hhea_descent_add = 0
+fMm.hhea_linegap = 0
 
 # marge ReplaceParts
 print "marge ReplaceParts"
@@ -335,19 +334,19 @@ target = (
     0x006c,  # l : small letter l
     0x2013,  # – : en dash –
     0x2014)  # — : em dash —
-copyAndPaste(fPa, target, fRm, target)
+copyAndPaste(fPa, target, fMm, target)
 
 # marge Mgen+ 1m
 print "marge Mgen+ 1m"
-fRm.mergeFonts( srcfontMgenplus )
-fRm.os2_unicoderanges = fMg.os2_unicoderanges
-fRm.os2_codepages = fMg.os2_codepages
+fMm.mergeFonts( srcfontMgenplus )
+fMm.os2_unicoderanges = fMg.os2_unicoderanges
+fMm.os2_codepages = fMg.os2_codepages
 for l in fMg.gsub_lookups:
-    if (l in fRm.gsub_lookups) == False:
-        fRm.importLookups(fMg, l)
-for l in fRm.gsub_lookups:
+    if (l in fMm.gsub_lookups) == False:
+        fMm.importLookups(fMg, l)
+for l in fMm.gsub_lookups:
     if l.startswith(fMg.fontname + "-") == True:
-        fRm.removeLookup(l)
+        fMm.removeLookup(l)
 
 # marge Migu 1m
 print "marge Migu 1m"
@@ -358,10 +357,13 @@ target = (
     (0x30AB, 0x529B, 0x30A8, 0x5DE5, 0x30ED, 0x53E3, 0x30FC, 0x4E00, 0x30CB, 0x4E8C),
     # ～〜（FULLWIDTH TILDE・WAVE DASH）の区別
     (0xFF5E, 0x301C),
-    # αβなど一部のギリシャ文字、φЯなど一部のロシア文字を全角に
-    (rng(0x0391, 0x03a9), rng(0x03b1, 0x03c9)),
-    (0x0401, rng(0x0410, 0x044F), 0x0451))
-copyAndPaste(fMi, target, fRm, target)
+    # αβなど一部のギリシャ文字を全角に
+    list(u"ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψω"),
+    # φЯなど一部のキリル文字を全角に
+    list(u"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"),
+    # ±×÷√‰§†‡¶などの記号を全角に
+    list(u"±×÷√‰§†‡¶´¨‘’“”°′″"),)
+copyAndPaste(fMi, target, fMm, target)
 
 # modify
 print "modify"
@@ -369,43 +371,38 @@ print "modify"
 # scaling down
 if scalingDownIfWidth_flag == True:
     print "While scaling, wait a little..."
-    scalingDownIfWidth(fRm, newfont_em)
+    scalingDownIfWidth(fMm, newfont_em)
 
 
 # 全角 comma and period ，．
-select(fRm, 0xff0c, 0xff0e)
-fRm.transform(matMove(280, 0))
-fRm.transform(matRescale(512, 0, 1.54, 1.54))
-setWidth(fRm, newfont_em)
+select(fMm, 0xff0c, 0xff0e)
+fMm.transform(matMove(280, 0))
+fMm.transform(matRescale(512, 0, 1.54, 1.54))
+setWidth(fMm, newfont_em)
 
 # 全角 colon and semicolon ：；
-copyAndPaste    (fRm, 0xff0c, fRm, 0xff1b)
-copyAndPasteInto(fRm, 0xff0e, fRm, 0xff1b, 0, 410)
-copyAndPaste    (fRm, 0xff0e, fRm, 0xff1a)
-copyAndPasteInto(fRm, 0xff0e, fRm, 0xff1a, 0, 410)
-select(fRm, 0xff1b, 0xff1a)
+copyAndPaste    (fMm, 0xff0c, fMm, 0xff1b)
+copyAndPasteInto(fMm, 0xff0e, fMm, 0xff1b, 0, 410)
+copyAndPaste    (fMm, 0xff0e, fMm, 0xff1a)
+copyAndPasteInto(fMm, 0xff0e, fMm, 0xff1a, 0, 410)
+select(fMm, 0xff1b, 0xff1a)
 
 # 全角 brackets （）［］｛｝＜＞
 srcTarget = (0x0028, 0x0029, 0x005b, 0x005d, 0x007b, 0x007d, 0x003c, 0x003e)
 dstTarget = (0xff08, 0xff09, 0xff3b, 0xff3d, 0xff5b, 0xff5d, 0xff1c, 0xff1e)
-copyAndPaste(fRm, srcTarget, fRm, dstTarget)
-fRm.transform(matMove(256, 0))
-setWidth(fRm, newfont_em)
+copyAndPaste(fMm, srcTarget, fMm, dstTarget)
+fMm.transform(matMove(256, 0))
+setWidth(fMm, newfont_em)
 
 # post-process
-fRm.selection.all()
-fRm.round()
+fMm.selection.all()
+fMm.round()
 
+# generate
 print "Generate " + newfontM[0]
-fRm.generate(newfontM[0], '', generate_flags)
+fMm.generate(newfontM[0], '', generate_flags)
 
-#print "Generate " + newfontHM[0]
-#fRm.fontname   = newfontHM[1]
-#fRm.familyname = newfontHM[2]
-#fRm.fullname   = newfontHM[3]
-#fRm.sfnt_names = (('English (US)', 'UniqueID', newfontHM[2]), )
-#fRm.generate(newfontHM[0], '', generate_flags)
-
+fMm.close()
 fPa.close()
 fMg.close()
 fMi.close()
@@ -415,17 +412,17 @@ fMi.close()
 ########################################
 print
 print "Build " + newfontP[0]
-fRp = fontforge.open( newfontM[0] )
+fMp = fontforge.open( newfontM[0] )
 
 # pre-process
-fRp.fontname   = newfontP[1]
-fRp.familyname = newfontP[2]
-fRp.fullname   = newfontP[3]
-fRp.sfnt_names = (('English (US)', 'UniqueID', newfontP[2]), )
+fMp.fontname   = newfontP[1]
+fMp.familyname = newfontP[2]
+fMp.fullname   = newfontP[3]
+fMp.sfnt_names = (('English (US)', 'UniqueID', newfontP[2]), )
 
-panose = list(fRp.os2_panose)
+panose = list(fMp.os2_panose)
 panose[3] = 0
-fRp.os2_panose = tuple(panose)
+fMp.os2_panose = tuple(panose)
 
 # modify
 print "modify"
@@ -436,8 +433,8 @@ hankaku = (100,)
 zenkaku = (50,)
 
 # 全文字の幅の自動設定
-fRp.selection.all()
-for glyph in fRp.selection.byGlyphs:
+fMp.selection.all()
+for glyph in fMp.selection.byGlyphs:
     #print glyph.glyphname + ", code " + str(glyph.unicode) + ", width " + str(glyph.width)
     if glyph.width == newfont_em:         # 全角文字
         setAutoWidthGlyph(glyph, hankaku[0])
@@ -448,42 +445,36 @@ for glyph in fRp.selection.byGlyphs:
             setAutoWidthGlyph(glyph, zenkaku[0])
 
 # 半角数字は幅固定で中央配置
-select(fRp, rng(0x0030,0x0039))   # 0-9
-setWidth(fRp, 490)
-centerInWidth(fRp)
+select(fMp, rng(0x0030,0x0039))   # 0-9
+setWidth(fMp, 490)
+centerInWidth(fMp)
 
 # post-process
-fRp.selection.all()
-fRp.round()
+fMp.selection.all()
+fMp.round()
 
+# generate
 print "Generate " + newfontP[0]
-fRp.generate(newfontP[0], '', generate_flags)
+fMp.generate(newfontP[0], '', generate_flags)
 
-#print "Generate " + newfontHP[0]
-#fRp.fontname   = newfontHP[1]
-#fRp.familyname = newfontHP[2]
-#fRp.fullname   = newfontHP[3]
-#fRp.sfnt_names = (('English (US)', 'UniqueID', newfontHP[2]), )
-#fRp.generate(newfontHP[0], '', generate_flags)
-
-fRp.close()
+fMp.close()
 
 ########################################
 # create MyricaN
 ########################################
 print
 print "Build " + newfontN[0]
-fRn = fontforge.open( newfontM[0] )
+fMn = fontforge.open( newfontM[0] )
 
 # pre-process
-fRn.fontname   = newfontN[1]
-fRn.familyname = newfontN[2]
-fRn.fullname   = newfontN[3]
-fRn.sfnt_names = (('English (US)', 'UniqueID', newfontN[2]), )
+fMn.fontname   = newfontN[1]
+fMn.familyname = newfontN[2]
+fMn.fullname   = newfontN[3]
+fMn.sfnt_names = (('English (US)', 'UniqueID', newfontN[2]), )
 
-panose = list(fRn.os2_panose)
+panose = list(fMn.os2_panose)
 panose[3] = 0
-fRn.os2_panose = tuple(panose)
+fMn.os2_panose = tuple(panose)
 
 # modify
 print "modify"
@@ -502,8 +493,8 @@ zetc  = (0.60, 1.00, 50)
 hetc  = (0.85, 1.00, 50)
 
 # 拡大縮小 と 幅の自動設定
-fRn.selection.all()
-for glyph in fRn.selection.byGlyphs:
+fMn.selection.all()
+for glyph in fMn.selection.byGlyphs:
     #print glyph.glyphname + ", code " + str(glyph.unicode) + ", width " + str(glyph.width)
     glyph.ttinstrs = ""
     if glyph.unicode in zkana[3]:           # 全角かな
@@ -520,22 +511,16 @@ for glyph in fRn.selection.byGlyphs:
             setAutoWidthGlyph(glyph, hetc[2])
 
 # 半角数字は幅固定で中央配置
-select(fRn, rng(0x0030,0x0039))   # 0-9
-setWidth(fRn, 400)
-centerInWidth(fRn)
+select(fMn, rng(0x0030,0x0039))   # 0-9
+setWidth(fMn, 400)
+centerInWidth(fMn)
 
 # post-process
-fRn.selection.all()
-fRn.round()
+fMn.selection.all()
+fMn.round()
 
+# generate
 print "Generate " + newfontN[0]
-fRn.generate(newfontN[0], '', generate_flags)
+fMn.generate(newfontN[0], '', generate_flags)
 
-#print "Generate " + newfontHN[0]
-#fRn.fontname   = newfontHN[1]
-#fRn.familyname = newfontHN[2]
-#fRn.fullname   = newfontHN[3]
-#fRn.sfnt_names = (('English (US)', 'UniqueID', newfontHN[2]), )
-#fRn.generate(newfontHN[0], '', generate_flags)
-
-fRn.close()
+fMn.close()
