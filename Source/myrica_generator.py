1208424d9cbb9ad3838897673d33f27c2aef115e
munotoki <munotoki@gmail.com>
2015-01-02 21:08:37 +0900

    2.003.20150102
    2.003.20150102

--- Source/myrica_generator.py
+++ Source/myrica_generator.py
@@ -6,10 +6,8 @@
 #
 # This script is for generating ``myrica'' font
 #
-# * Inconsolata  : Inconsolata-Regular.ttf            : 1.013 (Google Fonts)
-# * 源真ゴシック : GenShinGothic-Monospace-Light.ttf  : 1.058.20140828
-# * (original)   : ReplaceParts.ttf                   : 1.006.20141102
-# * (original)   : HintingParts.ttf                   : 2.001.20141221
+# * Inconsolata  : Inconsolata-Regular.ttf                : 1.013 (Google Fonts)
+# * 源真ゴシック : GenShinGothic-Monospace-ExtraLight.ttf : 1.058.20140828
 #
 # 以下のように構成されます。
 # ・英数字記号は、Inconsolata
@@ -20,7 +18,7 @@
 #     ～〜（FULLWIDTH TILDE・WAVE DASH）の区別
 
 # version
-newfont_version      = "2.002.20141230"
+newfont_version      = "2.003.20150102"
 newfont_sfntRevision = 0x00010000
 
 # set font name
@@ -30,7 +28,7 @@ newfontN  = ("../Work/MyricaN.ttf", "MyricaN", "Myrica N", "Myrica Narrow")
 
 # source file
 srcfontIncosolata   = "../SourceTTF/Inconsolata-Regular.ttf"
-srcfontGenShin      = "../SourceTTF/GenShinGothic-Monospace-Light-ExpandH20.ttf"
+srcfontGenShin      = "../SourceTTF/GenShinGothic-Monospace-ExtraLight-ExpandH40.ttf"
 srcfontReplaceParts = "ReplaceParts.ttf"
 srcfontHintingParts = "HintingParts.ttf"
 
